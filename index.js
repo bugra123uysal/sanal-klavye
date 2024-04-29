df0a3a3aee5dc29934645a5d02fc5c77d7841a77
Mesut Buğra UYSAL <121828825+bugra123uysal@users.noreply.github.com>
2024-04-30 00:45:53 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function klavye(tuş){
+    document.getElementById("yz").value +=tuş
+     
+ 
+         }
+    /*  */
+         function tmz(){
+             document.getElementById("yz").value  ='' 
+         }
